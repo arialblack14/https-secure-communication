02fa7755c2c6b26a3a4c5753fae93321e9b69694
arialblack14 <tsirasalexis@yahoo.gr>
2016-03-27 19:13:27 +0300

    add facebook authentication

--- authenticate.js
+++ authenticate.js
@@ -1,5 +1,6 @@
 var passport = require('passport'),
-    LocalStrategy = require('passport-local').Strategy;
+    LocalStrategy = require('passport-local').Strategy,
+    FacebookStrategy = require('passport-facebook').Strategy;
 
 var User = require('./models/user');
 var config = require('./config');
@@ -8,3 +9,34 @@ exports.local = passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
+
+exports.facebook = passport.use(new FacebookStrategy({
+  clientID: config.facebook.clientID,
+  clientSecret: config.facebook.clientSecret,
+  callbackURL: config.facebook.callbackURL
+  },
+  function(accessToken, refreshToken, profile, done) {
+    User.findOne({ OauthId: profile.id }, function(err, user) {
+      if(err) {
+        console.log(err); // handle errors!
+      }
+      if (!err && user !== null) {
+        done(null, user);
+      } else {
+        user = new User({
+          username: profile.displayName
+        });
+        user.OauthId = profile.id;
+        user.OauthToken = accessToken;
+        user.save(function(err) {
+          if(err) {
+            console.log(err); // handle errors!
+          } else {
+            console.log("saving user ...");
+            done(null, user);
+          }
+        });
+      }
+    });
+  }
+));
--- routes/users.js
+++ routes/users.js
@@ -30,7 +30,7 @@ router.post('/register', function(req, res) {
           passport.authenticate('local')(req, res, function () {
               return res.status(200).json({status: 'Registration Successful!'});
           });
-        });        
+        });
     });
 });
 
@@ -66,6 +66,35 @@ router.get('/logout', function(req, res) {
   res.status(200).json({
     status: 'Bye!'
   });
+});
+
+router.get('/facebook', passport.authenticate('facebook'),
+  function(req, res){});
+
+router.get('/facebook/callback', function(req,res,next){
+  passport.authenticate('facebook', function(err, user, info) {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return res.status(401).json({
+        err: info
+      });
+    }
+    req.logIn(user, function(err) {
+      if (err) {
+        return res.status(500).json({
+          err: 'Could not log in user'
+        });
+      }
+              var token = Verify.getToken(user);
+              res.status(200).json({
+        status: 'Login successful!',
+        success: true,
+        token: token
+      });
+    });
+  })(req,res,next);
 });
 
 module.exports = router;
